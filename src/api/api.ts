1ce48bfc7ffa661e8e1241296d8d1abf1b02e9ff
Martine Enger <marenger@student.matnat.uio.no>
2018-06-18 15:48:25 +0200

    SD-365: Redirect til loginside ved 401 på kall mot backend.

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,33 @@
-const REST_API_URL: string = "http://localhost:8080/soknad-kontantstotte-api/status/isAlive";
+const REST_API_URL: string =  "http://localhost:8080/soknad-kontantstotte-api";
 
 const pingBackend = () => (
-    fetch(REST_API_URL).then((response) => response.json())
+    fetch(REST_API_URL).then((response) => {
+        if (response.status == 401) {
+            redirectTilLogin();
+            return;
+        }
+        return response.json();
+    })
 );
+
+const redirectTilLogin = () => {
+    window.location.href =
+        hentLoginUrl() + '?redirect=' + window.location.href;
+};
+
+// TODO: Disse bør inn i fasitvariabler
+const hentLoginUrl = () => {
+    if (window.location.href.indexOf('kontantstotte.nav.no') > -1) {
+        // Prod
+        return 'https://loginservice.nav.no/login'
+    } else if (window.location.href.indexOf('localhost') > -1) {
+        // Lokalt
+        return 'http://localhost:8080/soknad-kontantstotte-api/local/cookie'
+    } else {
+        // Preprod
+        return 'https://loginservice-q.nav.no/login'
+    }
+};
 
 const Api = { pingBackend };
 export default Api;
