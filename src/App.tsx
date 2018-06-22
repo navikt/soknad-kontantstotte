35429aaf0095262b806a8e7855d48ea9be95f13c
Martine Enger <marenger@student.matnat.uio.no>
2018-06-22 09:56:33 +0200

    [kaizen] fiks etter merge m master

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { Switch } from 'react-router-dom';
+import Api from "./api/api";
 import { renderSoknadRoutes } from './routes';
 
 const App = (): JSX.Element => {
+    Api.pingBackend();
     return (
         <div>
             <Switch>
