58d1c55400f4460049af21ddbe5b69c7b3db20d6
Ørjan Helstrøm <Orjan.Breimo.Helstrom@nav.no>
2018-06-22 10:31:27 +0200

    start-app * Vis spinner når appen starter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,49 @@
+import Spinner from 'nav-frontend-spinner';
 import * as React from 'react';
+import { connect } from 'react-redux';
 import { Switch } from 'react-router-dom';
+import { AppStatus } from './app/types';
+import { IRootState } from './rootReducer';
 import { renderSoknadRoutes } from './routes';
 
-const App = (): JSX.Element => {
-    return (
-        <div>
-            <Switch>
-                { renderSoknadRoutes() }
-            </Switch>
-        </div>
-    );
+interface IMapStateToProps {
+    status: AppStatus;
+}
+
+type Props = IMapStateToProps;
+
+const App: React.StatelessComponent<Props> = ({
+    status
+}) => {
+
+    switch (status) {
+        case AppStatus.IKKE_STARTET:
+        case AppStatus.STARTER:
+            return (
+                <Spinner type={'XXL'} />
+            );
+        case AppStatus.KLAR:
+            return (
+                <div>
+                    <Switch>
+                        { renderSoknadRoutes() }
+                    </Switch>
+                </div>
+            );
+        case AppStatus.FEILSITUASJON:
+            return (
+                <div>
+                    <p>En feil har oppstått</p>
+                </div>
+            );
+    }
+
 };
 
-export default App;
+const mapStateToProps = (state: IRootState): IMapStateToProps => {
+    return {
+        status: state.app.status
+    };
+};
+
+export default connect(mapStateToProps)(App);
