8b2ad60ce3545f6793f971400a42c13fd3b26e24
Martine Enger <marenger@student.matnat.uio.no>
2018-06-25 09:10:22 +0200

    SD-365: Støtte for backendkall og login i t og prod

--- src/Environment.ts
+++ src/Environment.ts
@@ -9,11 +9,21 @@ const Environment = (): IEnvUrls => {
             apiUrl: 'http://localhost:8080/soknad-kontantstotte-api',
             loginUrl: 'http://localhost:8080/soknad-kontantstotte-api/local/cookie'
         };
+    } else if (window.location.hostname.indexOf('soknad-kontantstotte-t') > -1) {
+        return {
+            apiUrl: 'https://soknad-kontantstotte-api-t.nav.no/soknad-kontantstotte-api',
+            loginUrl: 'https://loginservice-q.nav.no/login'
+        };
+    } else if (window.location.hostname.indexOf('soknad-kontantstotte-q') > -1) {
+        return {
+            apiUrl: 'https://soknad-kontantstotte-api-q.nav.no/soknad-kontantstotte-api',
+            loginUrl: 'https://loginservice-q.nav.no/login'
+        };
     }
 
     return {
-        apiUrl: 'https://soknad-kontantstotte-api-q.nav.no/soknad-kontantstotte-api',
-        loginUrl: 'https://loginservice-q.nav.no/login'
+        apiUrl: 'https://soknad-kontantstotte-api.nav.no/soknad-kontantstotte-api',
+        loginUrl: 'https://loginservice.nav.no/login'
     };
 };
 
