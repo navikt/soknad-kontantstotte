bd68651ae8a7c84ed08b06f5de096fece3be5186
Martine Enger <marenger@student.matnat.uio.no>
2018-06-25 14:51:38 +0200

    SD-365: Flytter backend-kall til rot-saga så det ikke ligger rett på App-komponenten

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import { Switch } from 'react-router-dom';
-import Api from "./api/api";
 import { renderSoknadRoutes } from './routes';
 
 const App = (): JSX.Element => {
-    Api.pingBackend();
     return (
         <div>
             <Switch>
