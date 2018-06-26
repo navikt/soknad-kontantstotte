a3c8735d69ed7a94e731b4a6b3dcb86df1b2951d
Nemanja Aksic <nemanja.aksic@bekk.no>
2018-06-26 13:49:17 +0200

    [SD-394] Legger til spørsmål på Familieforhold-siden - Utvider soknadsstaten med de nye feltene - Gjenbruker JaNeiSporsmal komponenten - Trekker ut spørsmålene/feltene om Annen Forelder i en egen komponent - Legger til tekstene til spørsmål og placeholder til feltene

--- src/sider/familieforhold/FamilieforholdSide.tsx
+++ src/sider/familieforhold/FamilieforholdSide.tsx
@@ -1,14 +1,54 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import JaNeiSporsmal from '../../component/JaNeiSporsmal/JaNeiSporsmal';
 import NavigasjonKnapp from '../../component/NavigasjonKnapp/NavigasjonKnapp';
 import SideContainer from '../../container/SideContainer/SideContainer';
+import { IRootState } from '../../rootReducer';
+import AnnenForelderInfo from './AnnenForelderInfo';
 
-const FamilieforholdSide = () => {
+interface IMapStateToProps {
+    borForeldreneSammenMedBarnet: boolean;
+    erAvklartDeltBosted?: boolean;
+}
+
+const FamilieforholdSide: React.StatelessComponent<IMapStateToProps> = (
+    {
+        borForeldreneSammenMedBarnet,
+        erAvklartDeltBosted,
+    }) => {
+
     return (
         <SideContainer>
             <h1>Bor begge foreldrene med barnet?</h1>
+
+            <JaNeiSporsmal
+                nokkel='borForeldreneSammenMedBarnet'
+                sporsmalNokkel='familieforhold.borForeldreneSammenMedBarnet.sporsmal'
+                verdi={ borForeldreneSammenMedBarnet }
+            />
+
+            { borForeldreneSammenMedBarnet === true &&
+                <AnnenForelderInfo />
+            }
+
+            { borForeldreneSammenMedBarnet === false &&
+                <JaNeiSporsmal
+                    nokkel='erAvklartDeltBosted'
+                    sporsmalNokkel='familieforhold.erAvklartDeltBosted.sporsmal'
+                    verdi={erAvklartDeltBosted}
+                />
+            }
+
             <NavigasjonKnapp to='/barnehageplass'>Neste</NavigasjonKnapp>
         </SideContainer>
     );
 };
 
-export default FamilieforholdSide;
+const mapStateToProps = (state: IRootState): IMapStateToProps => {
+    return {
+        borForeldreneSammenMedBarnet: state.soknad.borForeldreneSammenMedBarnet,
+        erAvklartDeltBosted: state.soknad.erAvklartDeltBosted,
+    };
+};
+
+export default connect(mapStateToProps)(FamilieforholdSide);
