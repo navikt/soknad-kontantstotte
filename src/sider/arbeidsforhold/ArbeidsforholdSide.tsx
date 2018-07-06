20da004533c1c7c98d8eaa5b0f4b1060209bd98d
Nemanja Aksic <nemanja.aksic@bekk.no>
2018-07-06 10:15:05 +0200

    [SD-413] Pakker arbeidsforhold innholdet inn i en ValidForm for å få validering på hele siden. - Bytter fra NavigasjonsKnapp til SubmitKnapp for å få til onsubmit logikken for skjemaet

--- src/sider/arbeidsforhold/ArbeidsforholdSide.tsx
+++ src/sider/arbeidsforhold/ArbeidsforholdSide.tsx
@@ -1,7 +1,9 @@
+import { push } from 'connected-react-router';
 import * as React from 'react';
-import { connect } from 'react-redux';
+import { connect, DispatchProp } from 'react-redux';
+import ValidForm from '../../common/lib/validation/ValidForm';
 import JaNeiSporsmal from '../../component/JaNeiSporsmal/JaNeiSporsmal';
-import NavigasjonKnapp from '../../component/NavigasjonKnapp/NavigasjonKnapp';
+import SubmitKnapp from '../../component/SubmitKnapp/SubmitKnapp';
 import SideContainer from '../../container/SideContainer/SideContainer';
 import { IRootState } from '../../rootReducer';
 import { Svar } from '../../soknad/reducer';
@@ -15,7 +17,7 @@ interface IMapStateToProps {
     mottarKontantstotteFraAnnetEOSForklaring?: string;
 }
 
-type ArbeidsforholdSideProps = IMapStateToProps;
+type ArbeidsforholdSideProps = IMapStateToProps & DispatchProp;
 
 const ArbeidsforholdSide: React.StatelessComponent<ArbeidsforholdSideProps> = (
     {
@@ -24,35 +26,35 @@ const ArbeidsforholdSide: React.StatelessComponent<ArbeidsforholdSideProps> = (
         arbeiderIUtlandetEllerKontinentalsokkel,
         arbeiderIUtlandetEllerKontinentalsokkelForklaring,
         mottarKontantstotteFraAnnetEOS,
-        mottarKontantstotteFraAnnetEOSForklaring
+        mottarKontantstotteFraAnnetEOSForklaring,
+        dispatch
     }) => {
     return (
         <SideContainer>
-            <JaNeiSporsmal
-                nokkel='mottarYtelserFraUtlandet'
-                sporsmalNokkel='arbeidsforhold.mottarYtelserFraUtlandet.sporsmal'
-                verdi={ mottarYtelserFraUtlandet }
-                harForklaring={ true }
-                forklaring={ mottarYtelserFraUtlandetForklaring }
-            />
-
-            <JaNeiSporsmal
-                nokkel='arbeiderIUtlandetEllerKontinentalsokkel'
-                sporsmalNokkel='arbeidsforhold.arbeiderIUtlandetEllerKontinentalsokkel.sporsmal'
-                verdi={ arbeiderIUtlandetEllerKontinentalsokkel }
-                harForklaring={ true }
-                forklaring={ arbeiderIUtlandetEllerKontinentalsokkelForklaring }
-            />
-
-            <JaNeiSporsmal
-                nokkel='mottarKontantstotteFraAnnetEOS'
-                sporsmalNokkel='arbeidsforhold.mottarKontantstotteFraAnnetEOS.sporsmal'
-                verdi={ mottarKontantstotteFraAnnetEOS }
-                harForklaring={ true }
-                forklaring={ mottarKontantstotteFraAnnetEOSForklaring }
-            />
-
-            <NavigasjonKnapp to='/oppsummering'>Neste</NavigasjonKnapp>
+            <ValidForm summaryTitle={'Arbeidsforhold'} onSubmit={() => dispatch(push('/oppsummering'))}>
+                <JaNeiSporsmal
+                    nokkel='mottarYtelserFraUtlandet'
+                    sporsmalNokkel='arbeidsforhold.mottarYtelserFraUtlandet.sporsmal'
+                    verdi={ mottarYtelserFraUtlandet }
+                    harForklaring={ true }
+                    forklaring={ mottarYtelserFraUtlandetForklaring }
+                />
+                <JaNeiSporsmal
+                    nokkel='arbeiderIUtlandetEllerKontinentalsokkel'
+                    sporsmalNokkel='arbeidsforhold.arbeiderIUtlandetEllerKontinentalsokkel.sporsmal'
+                    verdi={ arbeiderIUtlandetEllerKontinentalsokkel }
+                    harForklaring={ true }
+                    forklaring={ arbeiderIUtlandetEllerKontinentalsokkelForklaring }
+                />
+                <JaNeiSporsmal
+                    nokkel='mottarKontantstotteFraAnnetEOS'
+                    sporsmalNokkel='arbeidsforhold.mottarKontantstotteFraAnnetEOS.sporsmal'
+                    verdi={ mottarKontantstotteFraAnnetEOS }
+                    harForklaring={ true }
+                    forklaring={ mottarKontantstotteFraAnnetEOSForklaring }
+                />
+                <SubmitKnapp label='submitknapp.neste'/>
+            </ValidForm>
         </SideContainer>
     );
 };
