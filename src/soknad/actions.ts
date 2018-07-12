fe4a187c09b6e24e8fd3e8177c620cee4f5b1d88
Ørjan Helstrøm <Orjan.Breimo.Helstrom@nav.no>
2018-07-12 10:01:28 +0200

    innsending-del2 * Bytter ut bruk av ReturnType<> for typing av actions

--- src/soknad/actions.ts
+++ src/soknad/actions.ts
@@ -1,7 +1,19 @@
+import { Action } from 'redux';
 import { Bolk, Felt } from './types';
 
 enum SoknadTypeKeys {
     SETT_VERDI = 'SOKNAD_SETT_VERDI'
+}
+
+type SoknadActionTypes =
+    | ISoknadSettVerdi
+    ;
+
+interface ISoknadSettVerdi extends Action {
+    bolk: Bolk;
+    felt: Felt;
+    type: SoknadTypeKeys.SETT_VERDI;
+    verdi: any;
 }
 
 function soknadSettVerdi(bolk: Bolk, felt: Felt, verdi: any) {
@@ -12,9 +24,6 @@ function soknadSettVerdi(bolk: Bolk, felt: Felt, verdi: any) {
         verdi
     };
 }
-
-type SoknadActionTypes =
-    | ReturnType<typeof soknadSettVerdi>;
 
 export {
     SoknadActionTypes,
