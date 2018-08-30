bdbe74fe990e73c1e37f591a4a43ab08dfbbdfbc
Ørjan Helstrøm <Orjan.Breimo.Helstrom@nav.no>
2018-08-30 13:38:16 +0200

    enable-reg-test * Skrur på reg tester * Setter fast versjon av selenium og chrome * Måtte endre windowSize på chrome_small fordi chrome ikke lenger støttet å sette bredden til 430px

--- ci-tests/gemini/config.js
+++ ci-tests/gemini/config.js
@@ -1,20 +1,28 @@
 module.exports = {
     rootUrl: 'http://ci-test-server:8000',
-    gridUrl: 'http://hub:4444/wd/hub',
     screenshotsDir: './baseline',
-    desiredCapabilities: {
-        browserName: 'chrome',
-    },
 
     browsers: {
         chrome_large: {
+            gridUrl: 'http://selenium-hub:4444/wd/hub',
+            desiredCapabilities: {
+                browserName: 'chrome',
+            },
             windowSize: '1600x10000',
         },
         chrome_medium: {
+            gridUrl: 'http://selenium-hub:4444/wd/hub',
+            desiredCapabilities: {
+                browserName: 'chrome',
+            },
             windowSize: '800x10000',
         },
         chrome_small: {
-            windowSize: '430x10000',
+            gridUrl: 'http://selenium-hub:4444/wd/hub',
+            desiredCapabilities: {
+                browserName: 'chrome',
+            },
+            windowSize: '436x10000',
         },
     },
 
