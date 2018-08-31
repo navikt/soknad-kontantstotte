efe69e388314711b3a54e98f0517085703d33743
Henning Håkonsen <henninghaakonsen@me.com>
2018-08-31 10:34:48 +0200

    Rullet tilbake valideringssforslag da vi kanskje skal skrive om valideringen. Lagt på noe styling og fikset stylingproblemene som kom ved å endre på de generelle klassene for skjemaene

--- src/sider/krav-til-soker/KravTilSoker.tsx
+++ src/sider/krav-til-soker/KravTilSoker.tsx
@@ -1,8 +1,6 @@
-import KnappBase from 'nav-frontend-knapper';
 import Veilederpanel from 'nav-frontend-veilederpanel';
-import Veileder from 'nav-frontend-veileder';
 import * as React from 'react';
-import { FormattedMessage, InjectedIntlProps, injectIntl } from 'react-intl';
+import { InjectedIntlProps, injectIntl } from 'react-intl';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
 import { appNesteSteg } from '../../app/actions';
@@ -10,6 +8,7 @@ import ValidCheckboxPanelGruppe from '../../common/lib/validation/ValidCheckboxP
 import ValidForm from '../../common/lib/validation/ValidForm';
 import Veilederikon from '../../component/Ikoner/Veilederikon';
 import SideContainer from '../../component/SideContainer/SideContainer';
+import Submitknapp from '../../component/Submitknapp/Submitknapp';
 import { IRootState } from '../../rootReducer';
 import { soknadSettVerdi } from '../../soknad/actions';
 import { selectKravTilSoker } from '../../soknad/selectors';
@@ -32,139 +31,116 @@ const handterCheckboxEndring = (
 
 type KravTilSokerProps = IKravTilSoker & IMapDispatchToProps & InjectedIntlProps;
 
-interface IKravTilSokerState {
-    submitTrykket: boolean;
-}
-
-class KravTilSoker extends React.Component<KravTilSokerProps, IKravTilSokerState> {
-    constructor(props: KravTilSokerProps) {
-        super(props);
-
-        this.state = {
-            submitTrykket: false,
-        };
-    }
-
-    public render() {
-        const {
-            barnIkkeHjemme,
-            boddEllerJobbetINorgeSisteFemAar,
-            borSammenMedBarnet,
-            ikkeAvtaltDeltBosted,
-            norskStatsborger,
-            skalBoMedBarnetINorgeNesteTolvMaaneder,
-            intl,
-            settCheckboxVerdi,
-            nesteSteg,
-        } = this.props;
-
-        const valideringsFeil =
-            (harHuketAvPaCheckbox(norskStatsborger) ||
-                harHuketAvPaCheckbox(boddEllerJobbetINorgeSisteFemAar) ||
-                harHuketAvPaCheckbox(borSammenMedBarnet) ||
-                harHuketAvPaCheckbox(barnIkkeHjemme) ||
-                harHuketAvPaCheckbox(ikkeAvtaltDeltBosted) ||
-                harHuketAvPaCheckbox(skalBoMedBarnetINorgeNesteTolvMaaneder)) &&
-            this.state.submitTrykket;
-
-        return (
-            <SideContainer>
-                <div className="veileder">
-                    <Veilederpanel svg={<Veilederikon morkBakgrunn />} type="normal" kompakt={true}>
-                        <h4 className="krav__veileder__velkomstMelding">
-                            {intl.formatMessage({
-                                id: 'startside.krav.velkomstMelding',
-                            })}
-                        </h4>
-                        {intl.formatMessage({
-                            id: 'startside.krav.veileder',
-                        })}
-                    </Veilederpanel>
-                </div>
-                <ValidForm
-                    summaryTitle={'Søknad om kontantstøtte'}
-                    onSubmit={() => {
-                        nesteSteg();
-                        this.setState({ submitTrykket: false });
-                    }}
+const KravTilSoker: React.StatelessComponent<KravTilSokerProps> = ({
+    norskStatsborger,
+    boddEllerJobbetINorgeSisteFemAar,
+    borSammenMedBarnet,
+    barnIkkeHjemme,
+    ikkeAvtaltDeltBosted,
+    skalBoMedBarnetINorgeNesteTolvMaaneder,
+    intl,
+    settCheckboxVerdi,
+    nesteSteg,
+}) => {
+    return (
+        <SideContainer>
+            <div className="veileder">
+                <Veilederpanel
+                    svg={<Veilederikon morkBakgrunn={true} />}
+                    type="normal"
+                    kompakt={true}
                 >
-                    <ValidCheckboxPanelGruppe
-                        className="krav"
-                        legend={''}
-                        checkboxes={[
-                            {
-                                checked: norskStatsborger === Svar.JA,
-                                label: intl.formatMessage({
-                                    id: 'startside.krav.norskStatsborger',
-                                }),
-                                value: 'norskStatsborger',
-                            },
-                            {
-                                checked: boddEllerJobbetINorgeSisteFemAar === Svar.JA,
-                                label: intl.formatMessage({
-                                    id: 'startside.krav.boddEllerJobbetINorgeSisteFemAar',
-                                }),
-                                value: 'boddEllerJobbetINorgeSisteFemAar',
-                            },
-                            {
-                                checked: borSammenMedBarnet === Svar.JA,
-                                label: intl.formatMessage({
-                                    id: 'startside.krav.borSammenMedBarnet',
-                                }),
-                                value: 'borSammenMedBarnet',
-                            },
-                            {
-                                checked: barnIkkeHjemme === Svar.JA,
-                                label: intl.formatMessage({ id: 'startside.krav.barnIkkeHjemme' }),
-                                value: 'barnIkkeHjemme',
-                            },
-                            {
-                                checked: ikkeAvtaltDeltBosted === Svar.JA,
-                                label: intl.formatMessage({
-                                    id: 'startside.krav.ikkeAvtaltDeltBosted',
-                                }),
-                                value: 'ikkeAvtaltDeltBosted',
-                            },
-                            {
-                                checked: skalBoMedBarnetINorgeNesteTolvMaaneder === Svar.JA,
-                                label: intl.formatMessage({
-                                    id: 'startside.krav.skalBoMedBarnetINorgeNesteTolvMaaneder',
-                                }),
-                                value: 'skalBoMedBarnetINorgeNesteTolvMaaneder',
-                            },
-                        ]}
-                        onChange={(event: any, value: any) => {
-                            handterCheckboxEndring(event, settCheckboxVerdi, value);
-                        }}
-                        validators={[
-                            {
-                                failText: intl.formatMessage({ id: 'svar.feilmeldingCheckbox' }),
-                                test: () => harHuketAvPaCheckbox(norskStatsborger),
-                            },
-                            {
-                                failText: intl.formatMessage({ id: 'svar.feilmeldingCheckbox' }),
-                                test: () => harHuketAvPaCheckbox(boddEllerJobbetINorgeSisteFemAar),
-                            },
-                            {
-                                failText: intl.formatMessage({ id: 'svar.feilmeldingCheckbox' }),
-                                test: () => harHuketAvPaCheckbox(borSammenMedBarnet),
-                            },
-                            {
-                                failText: intl.formatMessage({ id: 'svar.feilmeldingCheckbox' }),
-                                test: () => harHuketAvPaCheckbox(barnIkkeHjemme),
-                            },
-                            {
-                                failText: intl.formatMessage({ id: 'svar.feilmeldingCheckbox' }),
-                                test: () => harHuketAvPaCheckbox(ikkeAvtaltDeltBosted),
-                            },
-                            {
-                                failText: intl.formatMessage({ id: 'svar.feilmeldingCheckbox' }),
-                                test: () =>
-                                    harHuketAvPaCheckbox(skalBoMedBarnetINorgeNesteTolvMaaneder),
-                            },
-                        ]}
-                    />
-                    {valideringsFeil && (
+                    <h4 className="krav__veileder__velkomstMelding">
+                        {intl.formatMessage({
+                            id: 'startside.krav.velkomstMelding',
+                        })}
+                    </h4>
+                    {intl.formatMessage({
+                        id: 'startside.krav.veileder',
+                    })}
+                </Veilederpanel>
+            </div>
+            <ValidForm summaryTitle={'Søknad om kontantstøtte'} onSubmit={nesteSteg}>
+                <ValidCheckboxPanelGruppe
+                    className="krav"
+                    legend={''}
+                    checkboxes={[
+                        {
+                            checked: norskStatsborger === Svar.JA,
+                            label: intl.formatMessage({
+                                id: 'startside.krav.norskStatsborger',
+                            }),
+                            value: 'norskStatsborger',
+                        },
+                        {
+                            checked: boddEllerJobbetINorgeSisteFemAar === Svar.JA,
+                            label: intl.formatMessage({
+                                id: 'startside.krav.boddEllerJobbetINorgeSisteFemAar',
+                            }),
+                            value: 'boddEllerJobbetINorgeSisteFemAar',
+                        },
+                        {
+                            checked: borSammenMedBarnet === Svar.JA,
+                            label: intl.formatMessage({
+                                id: 'startside.krav.borSammenMedBarnet',
+                            }),
+                            value: 'borSammenMedBarnet',
+                        },
+                        {
+                            checked: barnIkkeHjemme === Svar.JA,
+                            label: intl.formatMessage({ id: 'startside.krav.barnIkkeHjemme' }),
+                            value: 'barnIkkeHjemme',
+                        },
+                        {
+                            checked: ikkeAvtaltDeltBosted === Svar.JA,
+                            label: intl.formatMessage({
+                                id: 'startside.krav.ikkeAvtaltDeltBosted',
+                            }),
+                            value: 'ikkeAvtaltDeltBosted',
+                        },
+                        {
+                            checked: skalBoMedBarnetINorgeNesteTolvMaaneder === Svar.JA,
+                            label: intl.formatMessage({
+                                id: 'startside.krav.skalBoMedBarnetINorgeNesteTolvMaaneder',
+                            }),
+                            value: 'skalBoMedBarnetINorgeNesteTolvMaaneder',
+                        },
+                    ]}
+                    onChange={(event: any, value: any) => {
+                        handterCheckboxEndring(event, settCheckboxVerdi, value);
+                    }}
+                    validators={[
+                        {
+                            failText: intl.formatMessage({ id: 'svar.feilmeldingCheckbox' }),
+                            test: () => harHuketAvPaCheckbox(norskStatsborger),
+                        },
+                        {
+                            failText: intl.formatMessage({ id: 'svar.feilmeldingCheckbox' }),
+                            test: () => harHuketAvPaCheckbox(boddEllerJobbetINorgeSisteFemAar),
+                        },
+                        {
+                            failText: intl.formatMessage({ id: 'svar.feilmeldingCheckbox' }),
+                            test: () => harHuketAvPaCheckbox(borSammenMedBarnet),
+                        },
+                        {
+                            failText: intl.formatMessage({ id: 'svar.feilmeldingCheckbox' }),
+                            test: () => harHuketAvPaCheckbox(barnIkkeHjemme),
+                        },
+                        {
+                            failText: intl.formatMessage({ id: 'svar.feilmeldingCheckbox' }),
+                            test: () => harHuketAvPaCheckbox(ikkeAvtaltDeltBosted),
+                        },
+                        {
+                            failText: intl.formatMessage({ id: 'svar.feilmeldingCheckbox' }),
+                            test: () =>
+                                harHuketAvPaCheckbox(skalBoMedBarnetINorgeNesteTolvMaaneder),
+                        },
+                    ]}
+                />
+                {/*this.state.failedSubmit &&
+                    !this.state.valid &&
+                    !noSummary &&
+                    advarselTekst !== undefined && (
                         <Veileder
                             className="krav"
                             type="advarsel"
@@ -178,21 +154,12 @@ class KravTilSoker extends React.Component<KravTilSokerProps, IKravTilSokerState
                         >
                             <Veilederikon morkBakgrunn={true} />
                         </Veileder>
-                    )}
-                    <KnappBase
-                        onClick={() => {
-                            this.setState({ submitTrykket: true });
-                        }}
-                        type="hoved"
-                        htmlType="submit"
-                    >
-                        <FormattedMessage id={'app.neste'} />
-                    </KnappBase>
-                </ValidForm>
-            </SideContainer>
-        );
-    }
-}
+                    )*/}
+                <Submitknapp label="app.neste" />
+            </ValidForm>
+        </SideContainer>
+    );
+};
 
 const mapStateToProps = (state: IRootState): IKravTilSoker => {
     return selectKravTilSoker(state);
