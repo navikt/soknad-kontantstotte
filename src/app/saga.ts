c9764415e5289c48f3ebc295ae1dec6b13ae6227
Martine Enger <marenger@student.matnat.uio.no>
2018-09-10 15:04:54 +0200

    [kaizen] Fjernet testkode og less-filer til JaNeiSporsmal som vi ikke brukte likevel

--- src/app/saga.ts
+++ src/app/saga.ts
@@ -54,7 +54,7 @@ function* startAppSaga(): SagaIterator {
 }
 
 function* urlEndretSaga(action: ILocationChangeAction): SagaIterator {
-    /*const appSteg = yield select(selectAppSteg);
+    const appSteg = yield select(selectAppSteg);
     const naavaerendeSide = Object.values(stegConfig).find(
         (side: ISteg) => side.path === action.payload.location.pathname
     );
@@ -68,7 +68,7 @@ function* urlEndretSaga(action: ILocationChangeAction): SagaIterator {
     } else if (naavaerendeSide && naavaerendeSide.stegIndeks < appSteg) {
         yield put(appSettSteg(naavaerendeSide.stegIndeks));
         yield put(replace(naavaerendeSide.path));
-    }*/
+    }
 }
 
 function* tilStegSaga(steg: number) {
