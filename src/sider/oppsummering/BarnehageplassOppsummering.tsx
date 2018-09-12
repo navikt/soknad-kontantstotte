f00797f05efdb8ede41ba5de9656020abb5d4f8e
Henning Håkonsen <henninghaakonsen@me.com>
2018-09-12 15:16:57 +0200

    Mindre endringer på oppsummering. Regresjonstester

--- src/sider/oppsummering/BarnehageplassOppsummering.tsx
+++ src/sider/oppsummering/BarnehageplassOppsummering.tsx
@@ -1,9 +1,6 @@
-import { dagDatoNøkkel } from 'nav-datovelger/dist/datovelger/utils';
-import Element from 'nav-frontend-typografi/lib/element';
-import Normaltekst from 'nav-frontend-typografi/lib/normaltekst';
 import * as React from 'react';
-import { FormattedMessage, InjectedIntl } from 'react-intl';
-import { BarnehageplassVerdier, IBarnehageplass } from '../../soknad/types';
+import { InjectedIntl } from 'react-intl';
+import { IBarnehageplass, Svar } from '../../soknad/types';
 import OppsummeringsListeElement from './OppsummeringsListeElement';
 
 interface IBarnehageplassOppsummeringProps {
@@ -16,91 +13,13 @@ const BarnehageplassOppsummering: React.StatelessComponent<IBarnehageplassOppsum
     intl,
 }) => {
     switch (barnehageplass.harBarnehageplass.verdi) {
-        case BarnehageplassVerdier.Ja:
+        case Svar.JA:
             return (
                 <OppsummeringsListeElement
                     tekst={intl.formatMessage({ id: 'oppsummering.barnehageplass.harPlass' })}
-                >
-                    <ul>
-                        <li className="list-unstyled list-detaljer">
-                            <Normaltekst>
-                                <FormattedMessage id="oppsummering.barnehageplass.dato.label" />
-                            </Normaltekst>
-                            <Element>{dagDatoNøkkel(new Date(barnehageplass.dato.verdi))}</Element>
-
-                            <br />
-
-                            <Normaltekst>
-                                <FormattedMessage id="oppsummering.barnehageplass.kommune.label" />
-                            </Normaltekst>
-                            <Element>{barnehageplass.kommune.verdi}</Element>
-
-                            <br />
-
-                            <Normaltekst>
-                                <FormattedMessage id="oppsummering.barnehageplass.antallTimer.label" />
-                            </Normaltekst>
-                            <Element>{barnehageplass.antallTimer.verdi}</Element>
-                        </li>
-                    </ul>
-                </OppsummeringsListeElement>
+                />
             );
-        case BarnehageplassVerdier.JaSkalSlutte:
-            return (
-                <OppsummeringsListeElement
-                    tekst={intl.formatMessage({
-                        id: 'oppsummering.barnehageplass.harPlassMenSkalSlutte',
-                    })}
-                >
-                    <ul>
-                        <li className="list-unstyled list-detaljer">
-                            <Normaltekst>
-                                <FormattedMessage id="oppsummering.barnehageplass.dato.label" />
-                            </Normaltekst>
-                            <Element>{dagDatoNøkkel(new Date(barnehageplass.dato.verdi))}</Element>
-
-                            <br />
-
-                            <Normaltekst>
-                                <FormattedMessage id="oppsummering.barnehageplass.kommune.label" />
-                            </Normaltekst>
-                            <Element>{barnehageplass.kommune.verdi}</Element>
-
-                            <br />
-
-                            <Normaltekst>
-                                <FormattedMessage id="oppsummering.barnehageplass.antallTimer.label" />
-                            </Normaltekst>
-                            <Element>{barnehageplass.antallTimer.verdi}</Element>
-                        </li>
-                    </ul>
-                </OppsummeringsListeElement>
-            );
-        case BarnehageplassVerdier.NeiHarFaatt:
-            return (
-                <OppsummeringsListeElement
-                    tekst={intl.formatMessage({
-                        id: 'oppsummering.barnehageplass.neiMenHarFattPlass',
-                    })}
-                >
-                    <ul>
-                        <li className="list-unstyled list-detaljer">
-                            <Normaltekst>
-                                <FormattedMessage id="oppsummering.barnehageplass.dato.label" />
-                            </Normaltekst>
-                            <Element>{dagDatoNøkkel(new Date(barnehageplass.dato.verdi))}</Element>
-
-                            <br />
-
-                            <Normaltekst>
-                                <FormattedMessage id="oppsummering.barnehageplass.kommune.label" />
-                            </Normaltekst>
-                            <Element>{barnehageplass.kommune.verdi}</Element>
-                        </li>
-                    </ul>
-                </OppsummeringsListeElement>
-            );
-        case BarnehageplassVerdier.Nei:
+        case Svar.NEI:
             return (
                 <OppsummeringsListeElement
                     tekst={intl.formatMessage({ id: 'oppsummering.barnehageplass.harIkkePlass' })}
