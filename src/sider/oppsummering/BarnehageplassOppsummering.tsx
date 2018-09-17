30383a71fbe123c74fb72e838a739141854e94f7
Ørjan Helstrøm <Orjan.Breimo.Helstrom@nav.no>
2018-09-17 13:00:56 +0200

    SD-534-oppsummering * Fjerner tom streng

--- src/sider/oppsummering/BarnehageplassOppsummering.tsx
+++ src/sider/oppsummering/BarnehageplassOppsummering.tsx
@@ -35,8 +35,7 @@ const BarnehageplassOppsummering: React.StatelessComponent<IBarnehageplassOppsum
     return (
         <OppsummeringPanel>
             <Element>
-                {' '}
-                <FormattedMessage id={'oppsummering.barnehageplass.tittel'} />{' '}
+                <FormattedMessage id={'oppsummering.barnehageplass.tittel'} />
             </Element>
             <SporsmalSvar
                 sporsmal={<FormattedMessage id={'oppsummering.barnehageplass.harBarnehageplass'} />}
