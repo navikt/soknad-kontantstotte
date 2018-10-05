37974d861769b46fa9c4982626a5b873dee1966a
Henning Håkonsen <henninghaakonsen@me.com>
2018-10-05 11:43:43 +0200

    Oppdaterte uploader image. La inn en div slik at testene feiler

--- ci-tests/scripts.sh
+++ ci-tests/scripts.sh
@@ -14,7 +14,7 @@ case "$1" in
         errorCode=$?
         if [ $errorCode -ne 0 ]; then
             docker-compose -p "$2" down
-            docker run -v `pwd`/reports:/upload/files navikt/docker-directory-upload:1.0.0 https://repo.adeo.no/repository/raw/nais/soknad-kontantstotte/
+            docker run -v `pwd`/reports:/upload/files navikt/docker-directory-uploader:1.0.0 https://repo.adeo.no/repository/raw/nais/soknad-kontantstotte/
             echo "Se feilrapport fra Gemini på https://repo.adeo.no/repository/raw/nais/soknad-kontantstotte/gemini/index.html"
             exit $errorCode
         fi
