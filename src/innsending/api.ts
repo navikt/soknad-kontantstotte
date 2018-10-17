0be3d527e233fb46934aa0d60c01b25fe3264f72
Nemanja Aksic <nemanja.aksic@bekk.no>
2018-10-17 09:31:32 +0200

    [SD-473] Tar i bruk moment.js * I stedet for å måtte håndtere timestamp selv, tar vi i bruk moment og moment-timezone biblioteket. * Gir muligheten til å kunne sette formatering basert på språk og vise dato basert på brukerens lokale tid i stedet for servertid. * Oppdaterer Webpack-common config-en til å ignorere alle locales bortsett fra engelsk, nynorsk og bokmål

--- src/innsending/api.ts
+++ src/innsending/api.ts
@@ -1,34 +1,6 @@
 import axios from 'axios';
+import * as moment from 'moment-timezone';
 import Environment from '../Environment';
-
-const formaterDato = (dato: string) => {
-    const datoObjekt = new Date(dato);
-    const manedListe = [
-        'januar',
-        'februar',
-        'mars',
-        'april',
-        'mai',
-        'juni',
-        'juli',
-        'august',
-        'september',
-        'oktober',
-        'november',
-        'desember',
-    ];
-    const dag = datoObjekt.getDate();
-    const maned = manedListe[datoObjekt.getMonth()];
-    const aar = datoObjekt.getFullYear();
-    const timer =
-        datoObjekt.getHours() < 10 ? '0' + datoObjekt.getHours().toString() : datoObjekt.getHours();
-    const minutter =
-        datoObjekt.getMinutes() < 10
-            ? '0' + datoObjekt.getMinutes().toString()
-            : datoObjekt.getMinutes();
-
-    return `${dag}. ${maned} ${aar} kl. ${timer}:${minutter}`;
-};
 
 function sendInnSoknad(soknad: object) {
     const formData = new FormData();
@@ -43,7 +15,7 @@ function sendInnSoknad(soknad: object) {
             withCredentials: true,
         })
         .then(response => {
-            return formaterDato(response.data);
+            return moment(response.data).format('LLL');
         });
 }
 
