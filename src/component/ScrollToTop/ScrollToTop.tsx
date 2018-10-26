06dbd4b04f4e05d19e7e13417629ccf38acee9a3
Martine Enger <marenger@student.matnat.uio.no>
2018-10-26 14:29:59 +0200

    SD-658: Resetter fokus ved route change

--- src/component/ScrollToTop/ScrollToTop.tsx
+++ src/component/ScrollToTop/ScrollToTop.tsx
@@ -2,14 +2,26 @@ import * as React from 'react';
 import { RouteComponentProps, RouteProps, withRouter } from 'react-router';
 
 class ScrollToTop extends React.Component<RouteComponentProps<RouteProps>> {
+    private node: React.RefObject<HTMLDivElement>;
+    constructor(props: RouteComponentProps<RouteProps>) {
+        super(props);
+        this.node = React.createRef();
+    }
     public componentDidUpdate(prevProps: RouteProps) {
         if (this.props.location !== prevProps.location) {
             window.scrollTo(0, 0);
+            if (this.node.current) {
+                this.node.current.focus();
+            }
         }
     }
 
     public render() {
-        return this.props.children;
+        return (
+            <div ref={this.node} tabIndex={-1}>
+                {this.props.children}
+            </div>
+        );
     }
 }
 
