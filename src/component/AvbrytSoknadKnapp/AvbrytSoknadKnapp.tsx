48b69f8303efce36d516241a5367749355bb00fe
Nemanja Aksic <nemanja.aksic@bekk.no>
2018-11-20 08:47:14 +0100

    [SD-519] Refaktorerer avbryt knappen inni modalen, legger på metrikker før bruker sendes videre ut av søknaden for å kunne se hvor mange som faktisk benytter den for å avbryte. * Bruker fo-frontendlogger mekanismen til å få metrikker inn i Grafana

---
+++ src/component/AvbrytSoknadKnapp/AvbrytSoknadKnapp.tsx
@@ -0,0 +1,47 @@
+import { Fareknapp } from 'nav-frontend-knapper';
+import * as React from 'react';
+import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+import { selectAppSteg } from '../../app/selectors';
+import { IRootState } from '../../rootReducer';
+
+interface IAvbrytSoknadKnappProps {
+    className?: string;
+}
+
+interface IMapStateToProps {
+    steg: number;
+}
+
+type AvbrytSoknadKnappProps = IAvbrytSoknadKnappProps & IMapStateToProps;
+
+const loggMetrikkerOgAvbryt = (steg: number) => {
+    if (window.frontendlogger) {
+        window.frontendlogger.event(
+            'soknad-kontantstotte-status',
+            {},
+            { status: 'avbrutt', steg: steg }
+        );
+    }
+
+    window.location.href = 'https://tjenester.nav.no/dittnav/innlogget';
+};
+
+const AvbrytSoknadKnapp: React.StatelessComponent<AvbrytSoknadKnappProps> = ({
+    className,
+    steg,
+}) => {
+    return (
+        <Fareknapp className={className} onClick={() => loggMetrikkerOgAvbryt(steg)}>
+            <FormattedMessage id={'app.avbryt'} />
+        </Fareknapp>
+    );
+};
+
+const mapStateToProps = (state: IRootState): IMapStateToProps => {
+    return {
+        steg: selectAppSteg(state),
+    };
+};
+
+export default connect(mapStateToProps)(AvbrytSoknadKnapp);
