8ddb714a4ec26f1b0fc036ac95eee4d999c13416
Ørjan Helstrøm <orjan.breimo.helstrom@nav.no>
2019-01-30 09:57:05 +0100

    SD-781-filopplasting * Gjør om på oppsettet for ci-test-server slik at multer blir installert. Kreves for å håndtere multipart formdata i express. * Flytter precommit hook fra "scripts" til en egen "husky" config i package.json. * Legger til docker-compose.yml i .prettierignore - prettier prøvde å formatere filen

--- src/soknad/valideringsConfig.ts
+++ src/soknad/valideringsConfig.ts
@@ -29,19 +29,17 @@ import {
 } from './validators';
 
 interface IValideringsConfig {
-    arbeidIUtlandet: { [felt in arbeidIUtlandetFeltnavn]: Array<((felt: IFelt) => IFelt)> };
-    barnehageplass: { [felt in barnehageplassFeltnavn]: Array<((felt: IFelt) => IFelt)> };
-    familieforhold: { [felt in familieforholdFeltnavn]: Array<((felt: IFelt) => IFelt)> };
-    kravTilSoker: { [felt in kravTilSokerFeltnavn]: Array<((felt: IFelt) => IFelt)> };
-    mineBarn: { [felt in minebarnFeltnavn]: Array<((felt: IFelt) => IFelt)> };
-    utenlandskeYtelser: { [felt in utenlandskeYtelserFeltnavn]: Array<((felt: IFelt) => IFelt)> };
-    oppsummering: { [felt in oppsummeringFeltnavn]: Array<((felt: IFelt) => IFelt)> };
+    arbeidIUtlandet: { [felt in arbeidIUtlandetFeltnavn]: Array<(felt: IFelt) => IFelt> };
+    barnehageplass: { [felt in barnehageplassFeltnavn]: Array<(felt: IFelt) => IFelt> };
+    familieforhold: { [felt in familieforholdFeltnavn]: Array<(felt: IFelt) => IFelt> };
+    kravTilSoker: { [felt in kravTilSokerFeltnavn]: Array<(felt: IFelt) => IFelt> };
+    mineBarn: { [felt in minebarnFeltnavn]: Array<(felt: IFelt) => IFelt> };
+    utenlandskeYtelser: { [felt in utenlandskeYtelserFeltnavn]: Array<(felt: IFelt) => IFelt> };
+    oppsummering: { [felt in oppsummeringFeltnavn]: Array<(felt: IFelt) => IFelt> };
     utenlandskKontantstotte: {
-        [felt in utenlandskKontantstotteFeltnavn]: Array<((felt: IFelt) => IFelt)>
+        [felt in utenlandskKontantstotteFeltnavn]: Array<(felt: IFelt) => IFelt>
     };
-    tilknytningTilUtland: {
-        [felt in tilknytningTilUtlandFeltnavn]: Array<((felt: IFelt) => IFelt)>
-    };
+    tilknytningTilUtland: { [felt in tilknytningTilUtlandFeltnavn]: Array<(felt: IFelt) => IFelt> };
 }
 
 const valideringsConfig: IValideringsConfig = {
