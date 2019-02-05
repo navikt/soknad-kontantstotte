50dfbf30ae2b9e99d8b31bab06dc85d1f83a7913
Ørjan Helstrøm <orjan.breimo.helstrom@nav.no>
2019-02-05 13:32:19 +0100

    SD-778-vedlegg-slutte-barnehage * Legger til spesialhåndtering for Safari. Safari støtter ikke å åpne blobs uten å bruke window.open.

--- src/component/Vedlegg/VedleggForhandsvisning.tsx
+++ src/component/Vedlegg/VedleggForhandsvisning.tsx
@@ -1,3 +1,4 @@
+import * as Bowser from 'bowser';
 import Lukknapp from 'nav-frontend-lukknapp';
 import * as React from 'react';
 import { Document, Page } from 'react-pdf';
@@ -12,6 +13,8 @@ const VedleggForhandsvisning: React.FunctionComponent<IVedleggForhandsvisning> =
     vedlegg,
     onDelete,
 }) => {
+    const browser = Bowser.getParser(window.navigator.userAgent);
+    const erSafari = browser.is('safari');
     return (
         <ul className={'vedlegg-forhandsvisning__liste'}>
             {vedlegg.map(v => {
@@ -25,28 +28,58 @@ const VedleggForhandsvisning: React.FunctionComponent<IVedleggForhandsvisning> =
                         >
                             {'Slett vedlegg: ' + v.filnavn}
                         </Lukknapp>
-                        <a
-                            target={'_blank'}
-                            rel={'noopener noreferer'}
-                            href={window.URL.createObjectURL(v.fil)}
-                        >
-                            <div className={'vedlegg-forhandsvisning__bilde-container'}>
-                                {erPdf ? (
-                                    <Document
-                                        className={'vedlegg-forhandsvisning__pdf'}
-                                        file={v.fil}
-                                    >
-                                        <Page pageNumber={1} width={222} />
-                                    </Document>
-                                ) : (
-                                    <img
-                                        className={'vedlegg-forhandsvisning__bilde'}
-                                        alt={v.filnavn}
-                                        src={window.URL.createObjectURL(v.fil)}
-                                    />
-                                )}
-                            </div>
-                        </a>
+                        {erSafari ? (
+                            <a
+                                target={'_blank'}
+                                rel={'noopener noreferer'}
+                                href={'javascript:void(0)'}
+                                onClick={() => {
+                                    const blob = new Blob([v.fil], { type: v.fil.type });
+                                    const url = window.URL.createObjectURL(blob);
+                                    window.open(url, '_blank');
+                                }}
+                            >
+                                <div className={'vedlegg-forhandsvisning__bilde-container'}>
+                                    {erPdf ? (
+                                        <Document
+                                            className={'vedlegg-forhandsvisning__pdf'}
+                                            file={v.fil}
+                                        >
+                                            <Page pageNumber={1} width={222} />
+                                        </Document>
+                                    ) : (
+                                        <img
+                                            className={'vedlegg-forhandsvisning__bilde'}
+                                            alt={v.filnavn}
+                                            src={window.URL.createObjectURL(v.fil)}
+                                        />
+                                    )}
+                                </div>
+                            </a>
+                        ) : (
+                            <a
+                                target={'_blank'}
+                                rel={'noopener noreferer'}
+                                href={window.URL.createObjectURL(v.fil)}
+                            >
+                                <div className={'vedlegg-forhandsvisning__bilde-container'}>
+                                    {erPdf ? (
+                                        <Document
+                                            className={'vedlegg-forhandsvisning__pdf'}
+                                            file={v.fil}
+                                        >
+                                            <Page pageNumber={1} width={222} />
+                                        </Document>
+                                    ) : (
+                                        <img
+                                            className={'vedlegg-forhandsvisning__bilde'}
+                                            alt={v.filnavn}
+                                            src={window.URL.createObjectURL(v.fil)}
+                                        />
+                                    )}
+                                </div>
+                            </a>
+                        )}
                     </li>
                 );
             })}
