3ea360354108a98458db6aea5369445110f5948b
Henning Håkonsen <henninghaakonsen@me.com>
2019-08-15 11:19:03 +0200

    Forbedringer i config for visuelle tester.

--- dev-server/ci-server.js
+++ dev-server/ci-server.js
@@ -5,11 +5,11 @@ const fs = require('fs');
 
 const port = 8000;
 
-app.use('/', express.static(path.join(__dirname, '..', 'dist')));
+app.use('/', express.static(path.join(__dirname, '..', 'development')));
 
 app.get('*', (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(fs.readFileSync(path.join(__dirname, '/../dist/index.html')));
+    res.write(fs.readFileSync(path.join(__dirname, '/../development/index.html')));
     res.end();
 });
 
